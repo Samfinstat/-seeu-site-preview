65532d89c98d9ba77260cb9aba650ab68801acf8
Samfinstat <132460097+Samfinstat@users.noreply.github.com>
2026-07-20 15:14:03 +0800

    Replace encoded scripts with standard static files

--- assets-loader.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  const assets = window.SEEU_ASSETS || {};
-  document.querySelectorAll('img[src]').forEach((image) => {
-    const embeddedSource = assets[image.getAttribute('src')];
-    if (embeddedSource) image.setAttribute('src', embeddedSource);
-  });
-})();
